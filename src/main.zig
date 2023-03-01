b86c9b90f3f9d61a3aab23bdc9b84401cb63400e
Thomas Rory Gummerson <thomas@gummerson.no>
2023-03-01 15:33:04 +0100

    Add basic REPL

--- src/main.zig
+++ src/main.zig
@@ -4,6 +4,7 @@ const c = @cImport({
 });
 const Runtime = @import("qjs/Runtime.zig").Runtime;
 const Context = @import("qjs/Context.zig").Context;
+const Value = @import("qjs/Value.zig").Value;
 const jsTagToString = @import("qjs/Context.zig").jsTagToString;
 
 fn load(_ctx: ?*c.JSContext, path: ?[*:0]const u8, state: ?*anyopaque) callconv(.C) ?*c.JSModuleDef {
@@ -35,11 +36,40 @@ fn load(_ctx: ?*c.JSContext, path: ?[*:0]const u8, state: ?*anyopaque) callconv(
     ).val.u.ptr);
 }
 
+fn printException(ctx: Context(void, void), exc: Value(void, void)) !void {
+    const name = exc.prop(ctx, "name");
+    defer name.free(ctx);
+
+    const msg = exc.prop(ctx, "message");
+    defer msg.free(ctx);
+
+    const stack = exc.prop(ctx, "stack");
+    defer stack.free(ctx);
+
+    if (msg.val.tag != c.JS_TAG_STRING) {
+        const exc_str = try exc.toString(ctx);
+        defer exc.freeString(ctx, exc_str);
+
+        std.debug.print("\x1b[91mUnhandled exception ({s}): {s}\x1b[0m\n", .{ jsTagToString(exc.val.tag), exc_str });
+        return;
+    }
+    const name_str = try name.toString(ctx);
+    defer name.freeString(ctx, name_str);
+
+    const message_str = try msg.toString(ctx);
+    defer msg.freeString(ctx, message_str);
+
+    const stack_str = try stack.toString(ctx);
+    defer stack.freeString(ctx, stack_str);
+
+    std.debug.print("\x1b[91m{s}: {s}\n{s}\x1b[0m\n", .{ name_str, message_str, stack_str });
+}
+
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{
         .enable_memory_limit = true,
     }){
-        .requested_memory_limit = 1024 * 1024,
+        .requested_memory_limit = 100 * 1024 * 1024,
     };
     defer std.debug.assert(!gpa.deinit());
     const allocator = gpa.allocator();
@@ -60,7 +90,35 @@ pub fn main() !void {
         \\globalThis.console = { log };
     , "prelude", c.JS_EVAL_TYPE_MODULE).free(ctx);
 
-    for (args[1..]) |arg| {
+    if (args.len == 1) {
+        var line = std.ArrayList(u8).init(allocator);
+        defer line.deinit();
+        const glob = ctx.globalThis();
+        defer glob.free(ctx);
+        const console = glob.prop(ctx, "console");
+        defer console.free(ctx);
+        const log = console.prop(ctx, "log");
+        defer log.free(ctx);
+        while (true) : (line.clearRetainingCapacity()) {
+            if (c.JS_IsJobPending(rt.ptr) > 0) {
+                var ptr: ?*c.JSContext = null;
+                _ = c.JS_ExecutePendingJob(rt.ptr, &ptr);
+            }
+            std.io.getStdIn().reader().readUntilDelimiterArrayList(&line, '\n', 1024 * 1024) catch break;
+            try line.append(0);
+            var res = ctx.eval(line.items[0 .. line.items.len - 1 :0], "<repl>", c.JS_EVAL_TYPE_GLOBAL);
+            defer res.free(ctx);
+
+            if (res.val.tag == c.JS_TAG_EXCEPTION) {
+                const exc = ctx.getException();
+                defer exc.free(ctx);
+                try printException(ctx, exc);
+            } else {
+                const val = c.JS_Call(ctx.ptr, log.val, .{ .tag = c.JS_TAG_UNDEFINED, .u = .{ .ptr = null } }, 1, &res.val);
+                defer c.JS_FreeValue(ctx.ptr, val);
+            }
+        }
+    } else for (args[1..]) |arg| {
         const code = try std.fs.cwd().readFileAllocOptions(
             allocator,
             arg,
@@ -76,33 +134,7 @@ pub fn main() !void {
         if (result.val.tag == 6) {
             const exc = ctx.getException();
             defer exc.free(ctx);
-
-            const name = exc.prop(ctx, "name");
-            defer name.free(ctx);
-
-            const msg = exc.prop(ctx, "message");
-            defer msg.free(ctx);
-
-            const stack = exc.prop(ctx, "stack");
-            defer stack.free(ctx);
-
-            if (msg.val.tag != c.JS_TAG_STRING) {
-                const exc_str = try exc.toString(ctx);
-                defer exc.freeString(ctx, exc_str);
-
-                std.debug.print("\x1b[91mUnhandled exception ({s}): {s}\x1b[0m\n", .{ jsTagToString(exc.val.tag), exc_str });
-                return;
-            }
-            const name_str = try name.toString(ctx);
-            defer name.freeString(ctx, name_str);
-
-            const message_str = try msg.toString(ctx);
-            defer msg.freeString(ctx, message_str);
-
-            const stack_str = try stack.toString(ctx);
-            defer stack.freeString(ctx, stack_str);
-
-            std.debug.print("\x1b[91m{s}: {s}\n{s}\x1b[0m\n", .{ name_str, message_str, stack_str });
+            try printException(ctx, exc);
         }
 
         while (c.JS_IsJobPending(rt.ptr) > 0) {
